65494614083ad8d0c3589e0cd8b27197865e4580
Chandrashekar Giridharan <chandrashekar.c27@gmail.com>
2019-02-09 22:22:31 +0530

    player movement

--- allobj.py
+++ allobj.py
@@ -6,6 +6,9 @@ pygame.display.set_mode()
 from os import path
 img_dir = path.join(path.dirname(__file__), 'assests_img')
 
+WIDTH = 750
+HEIGHT = 275
+
 # colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -17,34 +20,133 @@ BLUE = (0, 0, 250)
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-
+        # size in pixels
+        # 41x43 for standing
+        # 40x43 for running
+        # 55x26 for ducking
         # load all images of sprite
+        self.player_jump = pygame.image.load(path.join(img_dir, "dino_jump.png")).convert()
+        self.player_run1 = pygame.image.load(path.join(img_dir, "dino_run_1.png")).convert()
+        self.player_run2 = pygame.image.load(path.join(img_dir, "dino_run_2.png")).convert()
+        self.player_duck1 = pygame.image.load(path.join(img_dir, "dino_duck_1.png")).convert()
+        self.player_duck2 = pygame.image.load(path.join(img_dir, "dino_duck_2.png")).convert()
+        self.image = self.player_jump
+        self.rect = self.image.get_rect()
+        self.rect.center = (40, HEIGHT - 38)
+        self.acc = 9.5  # 8 for small jump(adding later)
+        self.y_vel = 0
+        self.gravity = 0.5
+        self.last_update = 0
+        self.duck = False
+        self.current_frame = 0
+    # make player jump
 
-# the plant enemy. contains all sizes
+    def jump(self):
+        self.y_vel -= self.acc
+
+    def update(self):
+        # check if player is ducking
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_DOWN]:
+            self.duck = True
+        else:
+            self.duck = False
+        # check if player is in air
+        if self.rect.bottom < HEIGHT - 16:
+            self.image = pygame.transform.scale(self.player_jump, (41, 43))
+            prev_x = self.rect.centerx
+            prev_y = self.rect.bottom
+            self.rect = self.image.get_rect()
+            self.rect.centerx = prev_x
+            self.rect.bottom = prev_y
+        else:
+            # make player look like running
+            now = pygame.time.get_ticks()
+            if self.duck:
+                if now - self.last_update > 100:
+                    self.last_update = now
+                    self.current_frame += 1
+                    if self.current_frame % 2 == 0:
+                        self.image = pygame.transform.scale((self.player_duck1), (55, 26))
+                        prev_x = self.rect.centerx
+                        prev_y = self.rect.bottom
+                        self.rect = self.image.get_rect()
+                        self.rect.centerx = prev_x
+                        self.rect.bottom = prev_y
+                    else:
+                        self.image = pygame.transform.scale((self.player_duck2), (55, 26))
+                        prev_x = self.rect.centerx
+                        prev_y = self.rect.bottom
+                        self.rect = self.image.get_rect()
+                        self.rect.centerx = prev_x
+                        self.rect.bottom = prev_y
+            else:
+                if now - self.last_update > 100:
+                    self.last_update = now
+                    self.current_frame += 1
+                    if self.current_frame % 2 == 0:
+                        self.image = pygame.transform.scale((self.player_run1), (40, 43))
+                        prev_x = self.rect.centerx
+                        prev_y = self.rect.bottom
+                        self.rect = self.image.get_rect()
+                        self.rect.centerx = prev_x
+                        self.rect.bottom = prev_y
+                    else:
+                        self.image = pygame.transform.scale((self.player_run2), (40, 43))
+                        prev_x = self.rect.centerx
+                        prev_y = self.rect.bottom
+                        self.rect = self.image.get_rect()
+                        self.rect.centerx = prev_x
+                        self.rect.bottom = prev_y
+        # check if player jumps
+        if not self.duck:
+            if keystate[pygame.K_UP] or keystate[pygame.K_SPACE]:
+                if not self.rect.bottom < HEIGHT - 16:
+                    self.jump()
+        # make the player fall down
+        if self.rect.bottom < HEIGHT - 16:
+            self.y_vel += self.gravity
+        # change positon of player based on y_vel
+        self.rect.y += self.y_vel
+        # keep the player above the ground
+        if self.rect.bottom > HEIGHT - 16:
+            self.rect.bottom = HEIGHT - 16
+            self.y_vel = 0
+
+            # the plant enemy. contains all sizes
 
 
 class Plant(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-# the bird enemy. contains all levels
+# the bird enemy. contains all heights
 
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
+
+
+# platform png to show
+platform_img = pygame.image.load(path.join(img_dir, 'platform.png')).convert()
+platform_img.set_colorkey(PLATFORM_BLUE)
+
 # the main platform
 
 
 class Platform(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(path.join(img_dir, 'platform.png')).convert()
-        self.image.set_colorkey(PLATFORM_BLUE)
+        self.image = pygame.Surface((3000, 3))
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.top = y
         self.scrollx = 0
-        self.speed = 2
+        self.speed = 5
 # fuction to display text on the screen
 
 
--- run_game.py
+++ run_game.py
@@ -3,12 +3,12 @@ main .py script to run to play the game
 consists of the main game loop
 """
 import os
+import random
+import time
 # import other scripts
 from allobj import *
-# basic settings
-WIDTH = 750
-HEIGHT = 350
-FPS = 20
+# fps
+FPS = 60
 
 
 # set up the pygame window
@@ -26,13 +26,22 @@ pygame.display.set_icon(game_icon)
 # make a group that has all sprites
 all_sprites = pygame.sprite.Group()
 # the platform sprite
-main_platform = Platform()
-
+main_platform = Platform(WIDTH / 2, HEIGHT - 17)
+all_sprites.add(main_platform)
+# player sprite
+the_player = Player()
+#print(the_player.rect.bottom)
+all_sprites.add(the_player)
 # start the main game loop
 running = True
 alive = False
+platform_img = pygame.image.load(path.join(img_dir, "platform.png")).convert()
+platform_img.set_colorkey(PLATFORM_BLUE)
 # display start screen stuff
-screen.blit(main_platform.image, (0, HEIGHT - 20))
+screen.blit(platform_img, (0, HEIGHT - 20))
+all_sprites.draw(screen)
+# flip the dislay
+pygame.display.flip()
 while running:
 
     # check for inputs
@@ -48,21 +57,21 @@ while running:
         # white screen to draw on next frame
         screen.fill(WHITE)
         # keep the platform scrolling
-        rel_x = main_platform.scrollx % main_platform.rect.width
+        rel_x = main_platform.scrollx % 1189
         if rel_x < WIDTH:
-            screen.blit(main_platform.image, (rel_x, HEIGHT - 20))
+            screen.blit(platform_img, (rel_x, HEIGHT - 20))
         main_platform.scrollx -= main_platform.speed
-        screen.blit(main_platform.image, (rel_x - main_platform.rect.width, HEIGHT - 20))
-
-    #  Update
-    all_sprites.update()
-    #  Draw / render
-    all_sprites.draw(screen)
+        screen.blit(platform_img, (rel_x - 1189, HEIGHT - 20))
+        #  Update
+        all_sprites.update()
+        #  Draw / render
+        all_sprites.draw(screen)
+        # flip the dislay
+        pygame.display.flip()
     #  keep loop running at the right speed
     clock.tick(FPS)
-    print(str(clock.get_fps()))
-    # flip the dislay
-    pygame.display.flip()
+    # print(str(clock.get_fps()))
+    print(pygame.sprite.collide_rect_ratio(1)(main_platform, the_player))
 # quit pygame
 pygame.quit()
 quit()
